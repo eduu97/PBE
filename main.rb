0059643e921f212af8378071093e2dc501e18967
SoufianRF <36482776+SoufianRF@users.noreply.github.com>
2018-02-19 21:44:12 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,6 +6,11 @@ output_thread = Thread.new{output()}
 
 #Input goes here
 
+#cat /dev/pts/5
+teclado = gets(nil)
+aux = open('RUTA SERIAL PORT', 'w') { |f|
+  f << teclado
+}
 
 #End of output thread
 output_thread.join
