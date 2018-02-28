686fd5340bad4eca30b8aa9953afbc6602914a49
lluisbaro <lluis.baro.cayetano@gmail.com>
2018-02-28 16:57:40 +0100

    Penjat el miniterm_Profe

---
+++ miniterm_Profe.rb
@@ -0,0 +1,30 @@
+require 'serialport'
+
+if ARGV.size != 5
+	STDERR.print <<EOF
+	Usage: ruby #{$0} num_port bps nbits stopb parity
+EOF
+	exit(1)
+end
+
+parity = case ARGV[4]
+  when "E"
+    SerialPort::EVEN
+  when "O"
+    SerialPort::ODD
+  when "N"
+    SerialPort::NONE
+end
+
+sp = SerialPort.new(ARGV[0], ARGV[1].to_i, ARGV[2].to_i, ARGV[3].to_i, parity)
+
+Thread.new{
+	while (true) do
+		STDOUT.putc(sp.getc)
+	end
+}
+
+while ((char = STDIN.getc) != ?\C-D) do
+	sp.putc(char)
+end
+
