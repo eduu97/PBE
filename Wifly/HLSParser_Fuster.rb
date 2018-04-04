e3f9981794da93bdbe829fc53a629a1f3707a62a
Lluís Fuster <lluis.lfb@gmail.com>
2018-04-04 17:44:45 +0200

    Afegir creació classes

--- Wifly/HLSParser_Fuster.rb
+++ Wifly/HLSParser_Fuster.rb
@@ -3,10 +3,29 @@ require 'net/http' #net/http ja requereix 'uri'
 uri = URI(ARGV[0])
 playlist = Net::HTTP.get(uri) #descarreguem l'arxiu de la uri
 
+class MasterPlaylist {
+  #parametres importants
+}
+  
+class Playlist {
+  def initialize(arrayFragments)
+    @arrFra = arrayFragments
+  end
+}
+#la de fragment segurament no  cal, es poden anar llegint sobre la marxa
+class Fragment {
+  def initialize(seqNum, uri, duration)
+    @num = seqNum
+    @uri = uri
+    @duration=duration
+  end
+}
+  
 if (isMaster(playlist))
   #ya tal
-
+  
 else
+  pl = Playlist.new
   matches = playlist.scan(/#EXTINF:\d+.?\d*/)  #màgia regex
   durations = []  #no se segur si cal declarar
   for a in matches
